24394d238a3eb723ede67eb65e260ab524f4bc89
eduardopietre <pietre.eduardo@gmail.com>
2021-05-08 12:38:29 -0300

    Added One Mp4ToPDF Conversion.

---
+++ tests/Testing.h
@@ -0,0 +1,17 @@
+// Simple header wrote by me.
+// Boiler plate for testing.
+
+
+#include <iostream>
+#include <chrono>
+
+
+#define TESTING_TIME_EXECUTION(name, codeblock) {\
+	auto _tte_start = std::chrono::high_resolution_clock::now();\
+	{\
+		codeblock \
+	};\
+	auto _tte_stop = std::chrono::high_resolution_clock::now();\
+	auto _tte_duration = std::chrono::duration_cast<std::chrono::milliseconds>(_tte_stop - _tte_start).count();\
+	std::cout << "TESTING_TIME_EXECUTION '" << name << "' at line " << __LINE__  << " took "<< _tte_duration << " milliseconds." << std::endl;\
+}
--- tests/TestsMain.cpp
+++ tests/TestsMain.cpp
@@ -1,8 +1,20 @@
+#include "Testing.h"
+#include "../src/Mp4ToPDF.h"
+
 #include <iostream>
+
+
+std::string filename = "C:/Users/User/Desktop/desk/mp4topdf tests/test.mp4";
+std::string outfilename = filename + ".pdf";
+int frameskip = 100;
 
 
 int main(int argc, char* argv[]) {
 	std::cout << "Starting TestsMain" << std::endl;
+
+	TESTING_TIME_EXECUTION("One Mp4ToPDF Conversion", {
+		DoMp4ToPDF(filename, outfilename, frameskip);
+	});
 
 	return 0;
 }
